6c077e18c11774adecbd0b2e96821e87989f7661
Fernando Sanchez <ies.fernandosanchez@gmail.com>
2021-11-13 18:53:46 +0100

    actualizacion

---
+++ site/web/Tema2/Ejercicio5-EjerciciosBasicos/Ejercicio3/main.js
@@ -0,0 +1,7 @@
+function comprobar(usuario, clave){
+    if (usuario=="Gwenevere" && clave=="excalibur"){
+        prompt("Usuario y contraseña correctos. Puede ingresar a Camelot")
+    }else{
+        prompt("Acceso denegado")
+    }
+}
