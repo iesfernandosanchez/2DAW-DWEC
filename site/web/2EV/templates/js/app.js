5b2c3fac5cee7f803d7f9ea09d7867ca0e975791
Fernando Sánchez <fer@madforwebs.com>
2022-02-04 18:18:37 +0100

    templates

--- site/web/2EV/templates/js/app.js
+++ site/web/2EV/templates/js/app.js
@@ -1,5 +1,18 @@
 class App{
-    constructor(){
-        
+    constructor(collectionData, itemId){
+        var panel = document.querySelector(itemId);
+        var templatePersona = document.querySelector('#persona');
+        for (let index = 0; index < collectionData.length; index++) {
+            const element = collectionData[index];
+            var cloneItemPersona = document.importNode(templatePersona.content, true);
+ 
+            var namePersona = cloneItemPersona.querySelector('#name');
+            namePersona.innerHTML = element['name'];
+
+            var descriptionPersona = cloneItemPersona.querySelector('#description');
+            descriptionPersona.innerHTML = element['description'];
+
+            panel.appendChild(cloneItemPersona);            
+        }
     }
 }
