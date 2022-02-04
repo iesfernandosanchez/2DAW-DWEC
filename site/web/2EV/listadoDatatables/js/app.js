2c92a1c419210706fd88b4b2b69623d368f0a57b
Fernando Sánchez <fer@madforwebs.com>
2022-02-04 16:31:34 +0100

    datatables

--- site/web/2EV/listadoDatatables/js/app.js
+++ site/web/2EV/listadoDatatables/js/app.js
@@ -1,6 +1,12 @@
 //https://datos.madrid.es/egob/catalogo/201747-0-bibliobuses-bibliotecas.json
 class App{
+
+    
     constructor(){
+        this.bibliotecas = [];
+    }
+
+    pintaDatos(){
 
     }
 
@@ -9,8 +15,66 @@ class App{
         request.open('GET', url, true);
         request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
-            console.log(app.JSONtransformToObject(request.responseText));
-            //return app.JSONtransformToObject(request.responseText);            
+            var objectBiblio = app.JSONtransformToObject(request.responseText);
+            var biblioCollection = objectBiblio['@graph'];
+
+            biblioCollection.forEach(element => {
+                app.bibliotecas.push(
+                    [
+                        element['title'],
+                        element['address']['street-address']
+                    ])
+            })
+
+            /*
+            var datatableBibliotecas = select("#datatable-data");
+            for (let index = 0; index < app.bibliotecas.length; index++) {
+                const element = app.bibliotecas[index];
+                console.log(element);
+
+                var filaDatatable = '<tr><td>'+element['title']+'</td><td>'+element['address']+'</td></tr>';
+                datatableBibliotecas.innerHTML += filaDatatable;
+
+                
+            }*/
+
+
+            console.log(app.bibliotecas);
+
+             /**
+             * Initiate Datatables
+             */
+
+             let myData = {
+                "headings": [
+                    "Name",
+                    "Address"
+                ],
+                "data": app.bibliotecas
+                
+                };
+              const datatables = select('.datatable', true)
+              datatables.forEach(datatable => {
+                  new simpleDatatables.DataTable(datatable,{
+
+                        columns: [
+                            // Sort the second column in ascending order
+                            { select: 1, sort: "desc" },
+                        ],
+                      'paging':false,
+                      
+                      'data': myData,
+                      
+                    });
+              })
+
+
+              
+
+
+            console.log(app.bibliotecas);
+            
+            
         } else {
             // We reached our target server, but it returned an error
         }
